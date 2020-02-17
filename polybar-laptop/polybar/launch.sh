878ee2a02590bb2831b746cd710cd6d07432d88a
Cuan <cliffordcuan@gmail.com>
2020-02-17 06:28:32 +0200

    adds laptop config for i3 and polybar, fixes mpd stow structure

--- polybar-laptop/polybar/launch.sh
+++ polybar-laptop/polybar/launch.sh
@@ -7,7 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar qbar-top &
-polybar qbar-bottom
+polybar qbar-top
 
 echo "Polybar launched..."
