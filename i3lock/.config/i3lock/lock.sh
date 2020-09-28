2c49a0abff43392b8a146331e0806608f4ef5596
cjclifford <cliffordcuan@gmail.com>
2020-09-28 22:45:20 +0200

    enables i3lock tiling for multi-monitor setups

--- i3lock/.config/i3lock/lock.sh
+++ i3lock/.config/i3lock/lock.sh
@@ -6,4 +6,4 @@ RES=1920x1080
 
 
 ffmpeg -loglevel panic -f x11grab -video_size $RES -y -i $DISPLAY -i $ICON -filter_complex "boxblur=5:2,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $BACKGROUND
-i3lock -u -i $BACKGROUND
+i3lock -u -t -i $BACKGROUND
