509d60479680ff03c00c90956a8514c4131e584c
cjclifford <cliffordcuan@gmail.com>
2020-09-28 20:05:21 +0200

    Adds monaco font

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,15 +1,20 @@
 sudo pacman -S git xorg-server zsh xorg-xrandr xorg-xinit pyenv ruby noto-fonts rxvt-unicode feh dmenu i3lock
 
-mkdir ~/AUR \
-	&& echo "Installing Polybar" \
-	&& cd ~/AUR \
+mkdir ~/AUR
+echo "Installing Polybar"
+cd ~/AUR \
 	&& git clone https://aur.archlinux.org/polybar-git.git \
 	&& cd polybar-git \
-	&& makepkg -si \
-	&& echo "Installing Hack Nerd Font" \
-	&& cd ~/AUR \
+	&& makepkg -si
+echo "Installing Hack Nerd Font"
+cd ~/AUR \
 	&& git clone https://aur.archlinux.org/nerd-fonts-hack.git \
 	&& cd nerd-fonts-hack \
+	&& makepkg -si
+echo "Installing Monaco Font"
+cd ~/AUR \
+	&& git clone https://aur.archlinux.org/ttf-monaco.git \
+	&& cd ttf-monaco \
 	&& makepkg -si
 
 echo "Installing Oh My Zsh"
