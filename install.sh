edae6d8db3e28055f17ff0949a8bb614867de5c5
Cuan Clifford <cliffordcuan@gmail.com>
2022-06-25 20:40:33 +0200

    - removes feh wallpaper script - updates i3 config - removes polybar config - adds i3blocks config - removes stow scripts - adds kakoune config and colors

--- install.sh
+++
@@ -1,9 +0,0 @@
-mkdir ~/.config
-
-stow fehbg
-stow i3
-stow i3lock
-stow polybar
-stow vim
-stow xorg
-stow zsh
--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-stow -D fehbg
-stow -D i3
-stow -D i3lock
-stow -D polybar
-stow -D vim
-stow -D xorg
-stow -D zsh
